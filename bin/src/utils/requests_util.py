97f7395fe278186279e8d671fe741959c83b7375
Alec Masterson <alecjmasterson@gmail.com>
2024-05-31 17:06:13 -0500

    allow requests_util to do GET or POST

--- bin/src/utils/requests_util.py
+++ bin/src/utils/requests_util.py
@@ -3,32 +3,34 @@ from utils import LOGGER
 import requests
 
 
-def get(url: str, headers: dict = {}, params: dict = {}) -> Any:
+def exchange(url: str, method: str, headers: dict = {}, params: dict = {}) -> Any:
     """
-    Function for making an HTTP GET request with proper error handling.
+    Function for making an HTTP request with proper error handling.
     An exception is thrown if an error occurs or the returned HTTP status is not OK.
 
     Parameters
     ----------
     url : str
         The URL to make the HTTP request against.
-    headers : dict
-        Dictionary (optional) containing header information for the HTTP request.
-    params : dict
-        Dictionary (optional) containing query parameter information for the HTTP request.
+    method : str
+        Type of HTTP request (GET, POST, etc).
+    headers : dict (optional)
+        Header information to include in the HTTP request.
+    params : dict (optional)
+        Query parameters to include in the HTTP request.
 
     Returns
     -------
     Any - A deserialized JSON response.
     """
     try:
-        LOGGER.info(f"[HTTP] - url={url}, headers={headers}, params={params}")
-        response: requests.Response = requests.get(url, headers=headers, params=params)
+        LOGGER.info(f"[HTTP] - [{method}] - url={url}, headers={headers}, params={params}")
+        response: requests.Response = requests.request(url, headers=headers, method=method, params=params)
 
-        # Return the JSON deserialized response ONLY if the HTTP status was okay.
+        # Return the deserialized JSON response if the HTTP status was okay.
         if response.status_code == requests.codes.ok:
             return response.json()
 
-        raise Exception(f"<{response.status_code}> - {response.reason}")
+        raise Exception(f"<{response.status_code}> - {response.reason} - {response.text}")
     except Exception as e:
-        raise Exception(f"[HTTP] - Failed to GET, {e}")
+        raise Exception(f"[HTTP] - [{method}] - Failed, {e}")
