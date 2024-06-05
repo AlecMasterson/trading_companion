ee1707a0c97f5516f97f73d93cdc47ee02c7150c
Alec Masterson <alecjmasterson@gmail.com>
2024-06-04 21:35:30 -0500

    update_tickers in Polygon util

--- bin/src/utils/polygon.py
+++ bin/src/utils/polygon.py
@@ -1,5 +1,6 @@
 from enums.Granularity import Granularity
 from models.Candle import Candle
+from models.Ticker import Ticker
 from utils.decorators import rate_limit
 from utils.requests_util import exchange
 from typing import Any, Generator, List, Optional, Union
@@ -54,5 +55,44 @@ def get_history(ticker: str, granularity: Granularity, start_date: str, end_date
     response: List[Candle] = []
     for results in get_results(url):
         response += [to_candle(i) for i in results]
+
+    return response
+
+
+def get_ticker_market_cap(ticker: str) -> float:
+    params: dict = {
+        "apiKey": os.environ["POLYGON_KEY"]
+    }
+    url: str = __ENDPOINT_TICKERS + f"/{ticker}"
+
+    response: List[dict] = [i for i in get_results(url, params=params)]
+    assert len(response) == 1, f"More than 1 Response - ticker={ticker}, size={len(response)}"
+
+    return response[0]["market_cap"] if "market_cap" in response[0] else 0
+
+
+def get_tickers() -> List[Ticker]:
+    exchanges: List[str] = ["XNAS", "XNYS"]
+    params: dict = {
+        "market": "stocks",
+        "type": "CS"
+    }
+    url: str = __ENDPOINT_TICKERS
+
+    def to_ticker(entry: dict) -> Ticker:
+        return Ticker(
+            active=False,
+            exchange=entry["primary_exchange"],
+            market_cap=0,
+            name=entry["name"],
+            ticker=entry["ticker"],
+            type=entry["type"],
+            valid=entry["active"]
+        )
+
+    response: List[Ticker] = []
+    for exchange in exchanges:
+        for results in get_results(url, params={**params, "exchange": exchange}):
+            response += [to_ticker(i) for i in results]
 
     return response
