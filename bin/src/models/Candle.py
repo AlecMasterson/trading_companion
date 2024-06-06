53b7e9c83546ed56043bb10d315f86f9818ad549
Alec Masterson <alecjmasterson@gmail.com>
2024-06-06 15:53:44 -0500

    update Candle model

--- bin/src/models/Candle.py
+++ bin/src/models/Candle.py
@@ -1,17 +1,13 @@
 from dataclasses import dataclass
-from enums.Granularity import Granularity
-from enums.Source import Source
 
 
 @dataclass
 class Candle:
-    data_source: Source
+    close: float
+    granularity: str
+    high: float
+    low: float
+    open: float
     ticker: str
-    granularity: Granularity
-    candle_timestamp: int
-    interpolated: bool
-    price_close: float
-    price_high: float
-    price_low: float
-    price_open: float
+    timestamp: int
     volume: float
