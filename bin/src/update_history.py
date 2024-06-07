2d32ec21905aa0cc945495712e49e385ea504e44
Alec Masterson <alecjmasterson@gmail.com>
2024-06-07 16:37:25 -0500

    get latest date for "update_history"

--- bin/src/update_history.py
+++ bin/src/update_history.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+from datetime import datetime, timedelta
 from models.Candle import Candle
 from psycopg import Connection, Cursor
 from typing import List
@@ -26,13 +27,16 @@ select ticker from stocks.ticker where active = true;
 
 @database_connection
 def main(database_conn: Connection = None) -> None:
+    end_date: str = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    LOGGER.info(f"end_date={end_date}")
+
     tickers: List[tuple] = database_conn.execute(__SQL_SELECT_TICKERS).fetchall()
     tickers: List[str] = [ticker[0] for ticker in tickers]
     LOGGER.info(f"tickers.length={len(tickers)}")
 
     cursor: Cursor = database_conn.cursor()
     for ticker in tickers:
-        history: List[Candle] = get_history(ticker, "DAY", "2020-01-01", "2023-01-01")
+        history: List[Candle] = get_history(ticker, "DAY", "2020-01-01", end_date)
         LOGGER.info(f"ticker={ticker}, history.length={len(history)}")
 
         cursor.executemany(__SQL_INSERT, [asdict(i) for i in history])
