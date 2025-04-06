0f0711ced5b57865026abee48e18e37c3c5baf18
Alec Masterson <alecjmasterson@gmail.com>
2025-04-05 22:53:45 -0500

    logger tweaks

--- bin/src/utils/__init__.py
+++ bin/src/utils/__init__.py
@@ -1,34 +1,24 @@
-from datetime import datetime
-from logging.handlers import TimedRotatingFileHandler
+from logging import FileHandler
+from typing import Any, Optional
+from utils.date_util import get_now_eastern, to_string
 import logging
 import sys
 import uuid
 
-
 def __create_logger() -> logging.Logger:
-    """
-    Function for creating a logger object to be used across the application.
-
-    Returns
-    -------
-    logging.Logger - the logger object
-    """
     logger = logging.getLogger("main")
     logger.setLevel(logging.DEBUG)
 
-    fileName = f"./logs/{datetime.now().strftime('%Y-%m-%d')}.log"
-    fileFormatter = logging.Formatter("[%(asctime)s] - level=[%(levelname)s] - trace=[" + str(uuid.uuid4()) + "] - module=[%(module)s] - func=[%(funcName)s] - %(message)s")
-
-    fileHandler = TimedRotatingFileHandler(fileName, when="d", interval=1,  backupCount=5)
+    fileHandler = FileHandler(f"./logs/{to_string(get_now_eastern(), '%Y-%m-%d')}.log")
     fileHandler.setLevel(logging.INFO)
-    fileHandler.setFormatter(fileFormatter)
+    fileHandler.setFormatter(logging.Formatter("[%(asctime)s] - level=[%(levelname)s] - trace=[" + str(uuid.uuid4()) + "] - module=[%(module)s] - func=[%(funcName)s] - %(message)s"))
     logger.addHandler(fileHandler)
 
     streamHandler = logging.StreamHandler()
     streamHandler.setLevel(logging.DEBUG)
     logger.addHandler(streamHandler)
 
-    def exception_handler(exc_type, exc_value, exc_traceback):
+    def exception_handler(exc_type: Optional[type], exc_value: Optional[BaseException], exc_traceback: Optional[Any]) -> None:
         if issubclass(exc_type, KeyboardInterrupt):
             sys.__excepthook__(exc_type, exc_value, exc_traceback)
             return
@@ -38,6 +28,5 @@ def __create_logger() -> logging.Logger:
     sys.excepthook = exception_handler
 
     return logger
-
 
 LOGGER: logging.Logger = __create_logger()
