683897db22297de42520027d516b350971c71c38
Alec Masterson <alecjmasterson@gmail.com>
2025-04-05 22:51:23 -0500

    change request logging

--- bin/src/utils/requests_util.py
+++ bin/src/utils/requests_util.py
@@ -3,7 +3,7 @@ from utils import LOGGER
 import requests
 
 def exchange(url: str, method: str, headers: dict = {}, params: dict = {}) -> Any:
-    LOGGER.info(f"[HTTP] - [{method}] - url={url}, headers={headers}, params={params}")
+    LOGGER.info(f"method={method} url={url} headers={headers} params={params}")
     response: requests.Response = requests.request(method, url, headers=headers, params=params)
 
     if response.status_code == requests.codes.ok:
