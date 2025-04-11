579a790c1f526da7b14cc29901f6e882111578f6
Alec Masterson <alecjmasterson@gmail.com>
2025-04-10 19:07:13 -0500

    IndicatorConfig

--- bin/src/models/IndicatorCandle.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from pydantic import BaseModel
-from typing import List, Optional
-
-class IndicatorCandle(BaseModel):
-    timestamp: datetime
-    values: List[Optional[float]]
--- bin/src/models/IndicatorConfig.py
+++ bin/src/models/IndicatorConfig.py
@@ -1,13 +1,11 @@
-from enums.Granularity import Granularity
 from enums.Indicator import Indicator
 from pydantic import BaseModel, Field
 from typing import Optional
 
-class IndicatorRequest(BaseModel):
-    granularity: Granularity
+class IndicatorConfig(BaseModel):
+    id: str
     indicator: Indicator
     period: Optional[int] = None
     period_fast: Optional[int] = Field(alias="periodFast", default=None)
     period_signal: Optional[int] = Field(alias="periodSignal", default=None)
     period_slow: Optional[int] = Field(alias="periodSlow", default=None)
-    ticker: str
