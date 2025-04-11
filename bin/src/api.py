97ddf3d2dddf261d9d2d0a8e4ea975fcd9d2ee95
Alec Masterson <alecjmasterson@gmail.com>
2025-04-11 13:05:44 -0500

    combine ticket history apis

--- bin/src/api.py
+++ bin/src/api.py
@@ -1,15 +1,13 @@
-from enums.Granularity import Granularity
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models.Candle import Candle
-from models.IndicatorCandle import IndicatorCandle
-from models.IndicatorRequest import IndicatorRequest
+from models.EnrichedCandle import EnrichedCandle
+from models.TickerHistoryRequest import TickerHistoryRequest
 from sqlalchemy.sql import Select
 from sqlmodel import Session, select
 from typing import List, Tuple
 from utils.database import get_database_session
-from utils.ta import get_indicator_values
-
+from utils.ta import apply_indicator
 
 app: FastAPI = FastAPI(title="Trading Companion")
 
@@ -20,20 +18,20 @@ app.add_middleware(
     allow_origins=["*"]
 )
 
-
 @app.get("/api/tickers", response_model=List[str])
 def get_tickers(database_session: Session = Depends(get_database_session)) -> List[str]:
     statement: Select[Tuple[str]] = select(Candle.ticker).distinct().order_by(Candle.ticker)
     return database_session.exec(statement).all()
 
+@app.post("/api/tickers/history", response_model=List[EnrichedCandle])
+def get_ticker_history(
+    request: TickerHistoryRequest,
+    database_session: Session = Depends(get_database_session)
+) -> List[EnrichedCandle]:
+    statement: Select[Tuple[Candle]] = select(Candle).where(Candle.ticker == request.ticker, Candle.granularity == request.granularity).order_by(Candle.timestamp)
+    candles: List[Candle] = database_session.exec(statement).all()
 
-@app.get("/api/tickers/{ticker}/history/{granularity}", response_model=List[Candle])
-def get_ticker_history(ticker: str, granularity: Granularity, database_session: Session = Depends(get_database_session)) -> List[Candle]:
-    statement: Select[Tuple[Candle]]= select(Candle).where(Candle.ticker == ticker, Candle.granularity == granularity).order_by(Candle.timestamp)
-    return database_session.exec(statement).all()
+    candles_enriched: List[EnrichedCandle] = [EnrichedCandle(**candle.model_dump()) for candle in candles]
+    [apply_indicator(indicator_config, candles_enriched) for indicator_config in request.indicator_configs]
 
-
-@app.post("/api/tickers/indicator", response_model=List[IndicatorCandle])
-def get_indicator(request: IndicatorRequest, database_session: Session = Depends(get_database_session)) -> List[IndicatorCandle]:
-    candles: List[Candle] = get_ticker_history(request.ticker, request.granularity, database_session)
-    return get_indicator_values(request, candles)
+    return candles_enriched
