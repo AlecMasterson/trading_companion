6b0f65ba0cc21595323d9d819a3d5330789f1912
Alec Masterson <alecjmasterson@gmail.com>
2025-04-17 19:16:18 -0500

    cleaned up UI API

--- web/src/api/AppApi.ts
+++ web/src/api/AppApi.ts
@@ -1,43 +1,48 @@
-import axios, {AxiosResponse, Method} from 'axios';
+import axios, {AxiosRequestConfig, AxiosResponse} from 'axios';
 import {Granularity} from '../types/enums/Granularity';
 import {Candle} from '../types/Candle';
-import {IndicatorCandle} from '../types/IndicatorCandle';
 import {IndicatorConfig} from '../types/IndicatorConfig';
+import {NotificationManager} from '../NotificationManager';
 
-axios.defaults.baseURL = 'http://localhost:8080';
+interface ExchangeProps extends AxiosRequestConfig {
+  errorMessage: string;
+}
 
 export default class AppApi {
 
-  public static async getIndicator(ticker: string, granularity: Granularity, indicatorConfig: IndicatorConfig): Promise<IndicatorCandle[]> {
-    const payload: object = {
-      granularity: granularity.toUpperCase(),
-      indicator: indicatorConfig.indicator,
-      period: indicatorConfig.period,
-      periodFast: indicatorConfig.periodFast,
-      periodSignal: indicatorConfig.periodSignal,
-      periodSlow: indicatorConfig.periodSlow,
-      ticker
+  public static async getTickerHistory(ticker: string, granularity: Granularity, indicatorConfigs: IndicatorConfig[]): Promise<Candle[]> {
+    const props: ExchangeProps = {
+      data: {
+        granularity: granularity.toUpperCase(),
+        indicatorConfigs,
+        ticker
+      },
+      errorMessage: 'getting the ticker history',
+      method: 'POST',
+      url: '/api/tickers/history'
     };
 
-    return AppApi.request<IndicatorCandle[]>('POST', '/api/tickers/indicator', {}, payload);
+    return await AppApi.exchange<Candle[]>(props);
   }
 
   public static async getTickers(): Promise<string[]> {
-    return AppApi.request<string[]>('GET', '/api/tickers');
+    const props: ExchangeProps = {
+      errorMessage: 'getting the available tickers',
+      method: 'GET',
+      url: '/api/tickers'
+    };
+
+    return await AppApi.exchange<string[]>(props);
   }
 
-  public static async getTickerHistory(ticker: string, granularity: Granularity): Promise<Candle[]> {
-    return AppApi.request<Candle[]>('GET', `/api/tickers/${ticker}/history/${granularity.toUpperCase()}`);
-  }
-
-  private static async request<T>(method: Method, url: string, params: object = {}, payload: any = undefined): Promise<T> {
-    await new Promise((resolve: (_: unknown) => void): number => setTimeout(resolve, 1500));
-
-    const response: AxiosResponse = await axios.request({data: payload, method, params, url});
-    if (response.status >= 400) {
-      throw new Error(`[HTTP] - <${response.status}> - ${response.statusText}`);
+  private static async exchange<T>(props: ExchangeProps): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await axios.request<T>(props);
+      return response.data;
+    } catch (error: unknown) {
+      const message: string = `Unexpected error while ${props.errorMessage}`;
+      NotificationManager(message, {hideIconVariant: true, variant: 'error'});
+      throw new Error(message);
     }
-
-    return response.data;
   }
 }
