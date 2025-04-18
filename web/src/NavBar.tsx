fe44ffb197b4ffe945bf5ef2940310714ba28f1a
Alec Masterson <alecjmasterson@gmail.com>
2025-04-18 09:29:10 -0500

    simplified and improved navbar

--- web/src/NavBar.tsx
+++ web/src/NavBar.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import {AppBar, Box, IconButton, Tab, Tabs, Toolbar, Typography} from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import {AppBar, Tab, Tabs, Toolbar, Typography} from '@mui/material';
 
-export default function Navbar(): React.ReactElement {
-  const [tabActive, setTabActive] = React.useState<number>(0);
+export enum TabId {
+  MARKET_VIEW = 'Market View',
+  SCREENER = 'Screener'
+}
 
-  function onChangeTab(_: React.SyntheticEvent, tabNew: number): void {
-    setTabActive(tabNew);
-  }
+interface NavbarProps {
+  activeTabId: TabId;
+  setActiveTabId: (_: TabId) => void;
+}
+
+const TabIdOrder: TabId[] = [TabId.MARKET_VIEW, TabId.SCREENER];
+
+export default function Navbar(props: NavbarProps): React.ReactElement<NavbarProps> {
+  const onChangeTabId: (event: any, index: number) => void = React.useCallback((_: any, index: number): void => {
+    props.setActiveTabId(TabIdOrder[index]);
+  }, []);
 
   return (
     <React.Fragment>
       <AppBar>
         <Toolbar>
-          <IconButton sx={{display: {md: 'none'}}}>
-            <MenuIcon />
-          </IconButton>
-
-          <Typography sx={{display: {xs: 'none', md: 'block'}, flexGrow: 1}} variant='h6'>
+          <Typography sx={{flexGrow: 1}} variant='h6'>
             Trading Companion
           </Typography>
 
-          <Box sx={{display: {xs: 'none', md: 'block'}}}>
-            <Tabs onChange={onChangeTab} value={tabActive}>
-              <Tab label='Market View' />
-              <Tab label='Research Portal' />
-              <Tab label='About' />
-            </Tabs>
-          </Box>
+          <Tabs onChange={onChangeTabId} value={TabIdOrder.indexOf(props.activeTabId)}>
+            <Tab label='Market View' />
+            <Tab label='Screener' />
+          </Tabs>
         </Toolbar>
       </AppBar>
 
