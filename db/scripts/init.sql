79e562d3de6aa533d04437b3d92b5ee0cf1d238e
Alec Masterson <alecjmasterson@gmail.com>
2025-04-19 15:30:12 -0500

    init script tweaks

--- db/scripts/init.sql
+++ db/scripts/init.sql
@@ -1,25 +1,32 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA market_data;
-
-GRANT CONNECT ON DATABASE trading_companion TO PUBLIC;
-GRANT USAGE ON SCHEMA market_data TO PUBLIC;
-ALTER DEFAULT PRIVILEGES IN SCHEMA market_data GRANT SELECT ON TABLES to PUBLIC;
-
-CREATE USER ep_senex_r WITH PASSWORD 'password123';
-
-CREATE TABLE market_data.candles (
-	created_at TIMESTAMPTZ NOT NULL,
-	"close" NUMERIC NOT NULL,
-	granularity VARCHAR(20) NOT NULL,
-	high NUMERIC NOT NULL,
-	low NUMERIC NOT NULL,
-	"open" NUMERIC NOT NULL,
-	ticker VARCHAR(20) NOT NULL,
-	"timestamp" TIMESTAMPTZ NOT NULL,
-	updated_at TIMESTAMPTZ NOT NULL,
-	volume NUMERIC NOT NULL,
-	CONSTRAINT history_pk PRIMARY KEY (granularity, ticker, "timestamp")
-);
+CREATE SCHEMA IF NOT EXISTS market_data;
 
 CREATE USER ep_senex WITH PASSWORD 'superspecialproductionpassword123';
+CREATE USER ep_senex_r WITH PASSWORD 'password123';
+
+GRANT CONNECT ON DATABASE trading_companion TO ep_senex, ep_senex_r;
+
+ALTER ROLE ep_senex SET search_path TO market_data;
+ALTER ROLE ep_senex_r SET search_path TO market_data;
+
+GRANT USAGE ON SCHEMA market_data TO ep_senex, ep_senex_r;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA market_data TO ep_senex_r;
+ALTER DEFAULT PRIVILEGES IN SCHEMA market_data GRANT SELECT ON TABLES TO ep_senex_r;
+
 GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA market_data TO ep_senex;
+ALTER DEFAULT PRIVILEGES IN SCHEMA market_data GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON TABLES TO ep_senex;
+
+CREATE TABLE market_data.candles (
+	source VARCHAR(20) NOT NULL,
+	ticker VARCHAR(20) NOT NULL,
+	granularity VARCHAR(20) NOT NULL,
+	timestamp TIMESTAMPTZ NOT NULL,
+	close NUMERIC NOT NULL,
+	high NUMERIC NOT NULL,
+	low NUMERIC NOT NULL,
+	open NUMERIC NOT NULL,
+	volume NUMERIC NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	CONSTRAINT history_pk PRIMARY KEY (source, ticker, granularity, timestamp)
+);
