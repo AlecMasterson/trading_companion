f29403402315eaa94637362fea4bdcf2ac0ff25d
Alec Masterson <alecjmasterson@gmail.com>
2025-04-26 07:50:12 -0500

    change database connection method

--- bin/src/utils/database.py
+++ bin/src/utils/database.py
@@ -3,9 +3,16 @@ from sqlmodel import Session, create_engine
 from typing import Generator
 import os
 
-engine: Engine = create_engine(os.environ["POSTGRES_CONN_STR"])
+_database_name: str = os.environ["POSTGRES_DB"]
+_database_host: str = os.environ["POSTGRES_HOST"]
+_database_password: str = os.environ["POSTGRES_PASSWORD"]
+_database_port: str = os.environ["POSTGRES_PORT"]
+_database_user: str = os.environ["POSTGRES_USER"]
+_engine: Engine = create_engine(
+    f"postgresql://{_database_user}:{_database_password}@{_database_host}:{_database_port}/{_database_name}"
+)
 
 # TODO: is this really the best way to do database management?
 def get_database_session() -> Generator[Session, None, None]:
-    with Session(engine) as database_session:
+    with Session(_engine) as database_session:
         yield database_session
