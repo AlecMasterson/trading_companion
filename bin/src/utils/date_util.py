22b22efac90e0948fe13792fcdd4d1655e90c052
Alec Masterson <alecjmasterson@gmail.com>
2025-05-01 15:01:47 -0500

    add a from_datetime_str date utility function

--- bin/src/utils/date_util.py
+++ bin/src/utils/date_util.py
@@ -1,6 +1,9 @@
 from datetime import UTC, datetime
 from zoneinfo import ZoneInfo
 
+def from_datetime_str(datetime_str: str, format: str) -> datetime:
+    return datetime.strptime(datetime_str, format).replace(tzinfo=UTC)
+
 def from_timestamp(timestamp: int) -> datetime:
     return datetime.fromtimestamp(timestamp / 1000, tz=UTC)
 
