8e35cd69520c0e94728765c3854ae1dd0a559781
Alec Masterson <alecjmasterson@gmail.com>
2025-05-01 13:05:34 -0500

    add market_insights schema

--- db/scripts/init.sql
+++ db/scripts/init.sql
@@ -1,4 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS market_data;
+CREATE SCHEMA IF NOT EXISTS market_insights;
 CREATE SCHEMA IF NOT EXISTS public;
 
 CREATE USER ep_senex WITH PASSWORD 'superspecialproductionpassword123';
@@ -6,14 +7,14 @@ CREATE USER ep_senex_r WITH PASSWORD 'password123';
 
 GRANT CONNECT ON DATABASE trading_companion TO ep_senex, ep_senex_r;
 
-GRANT USAGE ON SCHEMA market_data TO ep_senex, ep_senex_r;
+GRANT USAGE ON SCHEMA market_data, market_insights TO ep_senex, ep_senex_r;
 GRANT USAGE ON SCHEMA public TO ep_senex;
 
-GRANT SELECT ON ALL TABLES IN SCHEMA market_data TO ep_senex_r;
-ALTER DEFAULT PRIVILEGES IN SCHEMA market_data GRANT SELECT ON TABLES TO ep_senex_r;
+GRANT SELECT ON ALL TABLES IN SCHEMA market_data, market_insights TO ep_senex_r;
+ALTER DEFAULT PRIVILEGES IN SCHEMA market_data, market_insights GRANT SELECT ON TABLES TO ep_senex_r;
 
-GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA market_data TO ep_senex;
-ALTER DEFAULT PRIVILEGES IN SCHEMA market_data GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON TABLES TO ep_senex;
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA market_data, market_insights TO ep_senex;
+ALTER DEFAULT PRIVILEGES IN SCHEMA market_data, market_insights GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON TABLES TO ep_senex;
 GRANT ALL ON SCHEMA public to ep_senex;
 
 CREATE TABLE market_data.candles (
@@ -40,12 +41,12 @@ CREATE TABLE market_data.tickers (
 	CONSTRAINT tickers_pk PRIMARY KEY (ticker)
 );
 
-CREATE TABLE market_data.news (
-	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE market_insights.news_articles (
+	url TEXT NOT NULL,
 	snippet TEXT NOT NULL,
 	timestamp TIMESTAMPTZ NOT NULL,
 	title TEXT NOT NULL,
-	url TEXT NOT NULL,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-	updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	CONSTRAINT news_pk PRIMARY KEY (url)
 );
