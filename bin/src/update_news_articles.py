c01a50a6231dd253b42ce09a728ab111e6834626
Alec Masterson <alecjmasterson@gmail.com>
2025-05-01 16:58:05 -0500

    extract database session

--- bin/src/update_news_articles.py
+++ bin/src/update_news_articles.py
@@ -9,21 +9,21 @@ from utils.database import get_database_session
 from utils.date_util import get_now_eastern, to_string
 from typing import List
 
-def update(database_session: Session, date: str) -> None:
+_database_session: Session = next(get_database_session())
+
+def update(date: str) -> None:
     news_articles: List[NewsArticle] = get_news_articles(date)
     news_articles_dict: List[dict] = [article.model_dump() for article in news_articles]
     LOGGER.info(f"date={date} news_articles.length={len(news_articles_dict)}")
 
-    database_session.exec(db_insert(NewsArticle).values(news_articles_dict).on_conflict_do_nothing())
-    database_session.commit()
+    _database_session.exec(db_insert(NewsArticle).values(news_articles_dict).on_conflict_do_nothing())
+    _database_session.commit()
 
 if __name__ == "__main__":
     parser: ArgumentParser = ArgumentParser()
     parser.add_argument("-d", default=0, type=int)
     total_days_in_past: int = parser.parse_args().d
 
-    database_session: Session = next(get_database_session())
-
     for days_in_past in range(0, total_days_in_past+1):
         date: str = to_string(get_now_eastern() - timedelta(days=days_in_past), format="%Y-%m-%d")
-        update(database_session, date)
+        update(date)
--- bin/src/update_tickers.py
+++ bin/src/update_tickers.py
@@ -7,15 +7,15 @@ from typing import List
 from utils import LOGGER
 from utils.database import get_database_session
 
-def update(database_session: Session) -> None:
+_database_session: Session = next(get_database_session())
+
+def update() -> None:
     tickers: List[Ticker] = get_tickers()
     tickers_dict: List[dict] = [ticker.model_dump() for ticker in tickers if ticker.type != TickerType.OTHER]
-    LOGGER.info(f"tickers.length={len(tickers)}")
+    LOGGER.info(f"tickers.length={len(tickers_dict)}")
 
-    database_session.exec(db_insert(Ticker).values(tickers_dict).on_conflict_do_nothing())
-    database_session.commit()
+    _database_session.exec(db_insert(Ticker).values(tickers_dict).on_conflict_do_nothing())
+    _database_session.commit()
 
 if __name__ == "__main__":
-    database_session: Session = next(get_database_session())
-
-    update(database_session)
+    update()
