cb877cd8912d9e980c82c98d083cc93df3ade822
Alec Masterson <alecjmasterson@gmail.com>
2025-05-01 15:14:04 -0500

    simplify update_tickers script

--- bin/src/update_tickers.py
+++ bin/src/update_tickers.py
@@ -1,25 +1,21 @@
+from enums.TickerType import TickerType
 from models.db.Ticker import Ticker
 from services.polygon import get_tickers
 from sqlalchemy.dialects.postgresql import insert as db_insert
-from sqlalchemy.sql import Executable
 from sqlmodel import Session
 from typing import List
 from utils import LOGGER
 from utils.database import get_database_session
 
-def main() -> None:
-    database_session: Session = next(get_database_session())
-
+def update(database_session: Session) -> None:
     tickers: List[Ticker] = get_tickers()
+    tickers_dict: List[dict] = [ticker.model_dump() for ticker in tickers if ticker.type != TickerType.OTHER]
     LOGGER.info(f"tickers.length={len(tickers)}")
-    if len(tickers) == 0:
-        return
 
-    tickers_dict: List[dict] = [ticker.model_dump() for ticker in tickers]
-    statement: Executable = db_insert(Ticker).values(tickers_dict).on_conflict_do_nothing()
-
-    database_session.execute(statement)
+    database_session.exec(db_insert(Ticker).values(tickers_dict).on_conflict_do_nothing())
     database_session.commit()
 
 if __name__ == "__main__":
-    main()
+    database_session: Session = next(get_database_session())
+
+    update(database_session)
