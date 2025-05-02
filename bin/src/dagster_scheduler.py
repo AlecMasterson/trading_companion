0ae79f37da676afad8066cb08a94a88f52adb735
Alec Masterson <alecjmasterson@gmail.com>
2025-05-02 11:32:21 -0500

    add news articles to dagster

--- bin/src/dagster_scheduler.py
+++ bin/src/dagster_scheduler.py
@@ -1,6 +1,15 @@
 from dagster import RepositoryDefinition, ScheduleDefinition, job, op, repository
+from src.update_news_articles import main as update_news_articles
 from src.update_ticker_candle_history import main as update_ticker_candle_history
 from src.update_tickers import main as update_tickers
+
+@op
+def update_news_articles_op() -> None:
+    update_news_articles()
+
+@job
+def update_news_articles_job() -> None:
+    update_news_articles_op()
 
 @op
 def update_ticker_candle_history_op() -> None:
@@ -21,8 +30,14 @@ def update_tickers_job() -> None:
 @repository
 def main_repository() -> RepositoryDefinition:
     return [
+        update_news_articles_job,
         update_ticker_candle_history_job,
         update_tickers_job,
+        ScheduleDefinition(
+            job=update_news_articles_job,
+            cron_schedule="0 0 * * *",
+            execution_timezone="America/New_York"
+        ),
         ScheduleDefinition(
             job=update_ticker_candle_history_job,
             cron_schedule="0 1 * * *",
